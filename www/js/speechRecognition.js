8fcd9c7952561b3c3a0f5ee7e83133e7c8c89adf
Mo Shi <Monica@Mos-MacBook-Air.local>
2014-07-29 10:54:10 -0400

    add speech function

---
+++ www/js/speechRecognition.js
@@ -0,0 +1,31 @@
+
+            function onDeviceReady(){
+                console.log("Device is ready");
+            }
+
+            function recognizeSpeech() {
+                var maxMatches = 1;
+                var promptString = "Speak now"; // optional
+                var language = "en-US";    
+                alert("almost there");                 // optional
+                window.plugins.speechrecognizer.startRecognize(function(result){
+                    alert(result);
+                    if(result=="add alarm"){
+                        clockApp.showView('editAlarm');
+                    }
+                }, function(errorMessage){
+                    console.log("Error message: " + errorMessage);
+                }, maxMatches, promptString, language);
+            }
+
+            // Show the list of the supported languages
+            function getSupportedLanguages() {
+                window.plugins.speechrecognizer.getSupportedLanguages(function(languages){
+                    // display the json array
+                    alert(languages);
+                }, function(error){
+                    alert("Could not retrieve the supported languages : " + error);
+                });
+            }
+
+            document.addEventListener("deviceready", onDeviceReady, true);
